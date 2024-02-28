a57b573505b47bac59ae63093ed1cbf65216ef5e
Valeriy-Gnybidenko <valeriy.gnybidenko@gmail.com>
2024-02-28 22:49:14 +0200

    feat: add button component

---
+++ components/TheButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface iProps {
+  buttonSize: 'large' | 'medium' | 'small'
+  variant: 'fill' | 'outlined' | 'soft'
+  disabled?: boolean
+}
+
+defineProps<iProps>()
+</script>
+
+<template>
+  <button
+    class="button"
+    :class="[`button--${variant}`, `button--${variant}-${buttonSize}`]"
+    :disabled="disabled"
+  >
+    <slot />
+  </button>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,10 @@
 <template>
   <main>
     <section>
-      <div class="container"><h1 class="22">Hello from Emotion!</h1></div>
+      <div class="container">
+        <h1 class="22">Hello from Emotion!</h1>
+        <TheButton :button-size="'medium'" :variant="'soft'">Button</TheButton>
+      </div>
     </section>
   </main>
 </template>
