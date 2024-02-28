cba412c36e3add0e1ee9603cc7bb59029e8c2b25
Valeriy-Gnybidenko <valeriy.gnybidenko@gmail.com>
2024-02-28 23:31:06 +0200

    feat: add badge component"

---
+++ components/TheBadge.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface iProps {
+  variant: 'fill' | 'outlined'
+}
+
+defineProps<iProps>()
+</script>
+
+<template>
+  <button class="badge" :class="[`badge--${variant}`]">
+    <slot />
+    <span>
+      <IconsClose />
+    </span>
+  </button>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
       <div class="container">
         <h1 class="22">Hello from Emotion!</h1>
         <TheButton :button-size="'medium'" :variant="'soft'">Button</TheButton>
+        <TheBadge :variant="'outlined'">Badge</TheBadge>
       </div>
     </section>
   </main>
