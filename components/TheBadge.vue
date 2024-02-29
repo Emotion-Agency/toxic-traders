274367bdbbc533563734c0c12ff0b8d8bbf5c7e7
Valeriy-Gnybidenko <valeriy.gnybidenko@gmail.com>
2024-02-29 10:18:50 +0200

    fix: badge component

--- components/TheBadge.vue
+++ components/TheBadge.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
 interface iProps {
   variant: 'fill' | 'outlined'
+  isButton?: boolean
 }
 
 defineProps<iProps>()
+
+const emit = defineEmits(['click'])
+
+const onClose = () => {
+  emit('click')
+}
 </script>
 
 <template>
-  <button class="badge" :class="[`badge--${variant}`]">
+  <button
+    class="badge"
+    :class="[`badge--${variant}`]"
+    :disabled="!isButton"
+    @click="onClose"
+  >
     <slot />
-    <span>
+    <span v-if="isButton">
       <IconsClose />
     </span>
   </button>
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
       <div class="container">
         <h1 class="22">Hello from Emotion!</h1>
         <TheButton :button-size="'medium'" :variant="'soft'">Button</TheButton>
-        <TheBadge :variant="'outlined'">Badge</TheBadge>
+        <TheBadge :variant="'fill'" :is-button="true">Badge</TheBadge>
       </div>
     </section>
   </main>
