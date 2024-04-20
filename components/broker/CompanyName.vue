3b271931ef096ed705b2ab4ab23b9bf178a68a21
Valeriy-Gnybidenko <valeriy.gnybidenko@gmail.com>
2024-04-20 23:34:01 +0300

    feat: update company name component

--- components/broker/CompanyName.vue
+++ components/broker/CompanyName.vue
@@ -21,11 +21,17 @@ onMounted(async () => {
 
 <template>
   <div class="company-name">
-    <TheAccordion title="Company Name">
-      <p v-for="(item, idx) in companyNames" :key="idx">
-        {{ item?.companyName }}
-      </p>
-      <p v-if="!companyNames.length">Company names is not found</p>
+    <TheAccordion title="Company Name" :is-inputs="true">
+      <div
+        v-for="(item, idx) in companyNames"
+        :key="idx"
+        class="company-name__item"
+      >
+        <p v-if="companyNames.length" class="company-name__text">
+          {{ item?.companyName }}
+        </p>
+        <p v-else class="company-name__text">Company names is not found</p>
+      </div>
     </TheAccordion>
   </div>
 </template>
