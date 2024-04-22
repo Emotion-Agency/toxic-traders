99c67f55e4c84057863d8c3697f05ee29beb3f85
Valeriy-Gnybidenko <valeriy.gnybidenko@gmail.com>
2024-04-22 13:06:27 +0300

    feat: add show more button to server addresses

--- components/broker/ServerAddresses.vue
+++ components/broker/ServerAddresses.vue
@@ -8,8 +8,26 @@ interface iProps {
 const props = defineProps<iProps>()
 
 const serverAddresses = ref<iServerAddress[]>([])
+const showAllAddresses = ref(false)
+const filteredServerAddresses = computed(() => {
+  return serverAddresses.value.filter(item => {
+    if (showAllAddresses.value === false) {
+      return serverAddresses.value[0] === item
+    } else {
+      return serverAddresses.value
+    }
+  })
+})
 
 const { getServerAddresses } = useBrokerServerAddresses()
+
+const showMoreServerAddresses = () => {
+  showAllAddresses.value = true
+}
+
+const showLessServerAddresses = () => {
+  showAllAddresses.value = false
+}
 
 onMounted(async () => {
   const serverAddressesRequest = await getServerAddresses(props.brokerId)
@@ -24,7 +42,7 @@ onMounted(async () => {
   <div class="server-addresses">
     <TheAccordion title="Server Addresses" :is-inputs="true">
       <div
-        v-for="(item, idx) in serverAddresses"
+        v-for="(item, idx) in filteredServerAddresses"
         :key="idx"
         class="server-addresses__item"
       >
@@ -35,6 +53,17 @@ onMounted(async () => {
       <div v-if="!serverAddresses?.length" class="server-addresses__item">
         <p class="server-addresses__text">Server address is not found</p>
       </div>
+
+      <OptionalButton
+        v-if="serverAddresses.length > 1 && !showAllAddresses"
+        @on-click="showMoreServerAddresses"
+        >Show more</OptionalButton
+      >
+      <OptionalButton
+        v-if="filteredServerAddresses.length > 1"
+        @on-click="showLessServerAddresses"
+        >Show less</OptionalButton
+      >
     </TheAccordion>
   </div>
 </template>
