4e581ae89ba19067cc9b6d868701d0dfb9a011e2
letmewin22 <lkosteckiy5@gmail.com>
2024-05-31 10:24:36 +0300

    refactor: add composable with pagination

--- composables/pagination.ts
+++ composables/pagination.ts
@@ -1,3 +1,60 @@
-export const usePagination = () => {
-  return {}
+import type { iInput } from '~/types'
+
+export const usePagination = (
+  initialCurrentPage = 1,
+  initialItemsCount = 25
+) => {
+  const currentPage = ref(initialCurrentPage)
+  const totalCountPages = ref(0)
+  const itemsCount = ref(initialItemsCount)
+  const searchValue = ref<string>('1')
+
+  const computedTotalPages = computed(() => {
+    return Math.floor(totalCountPages.value / itemsCount.value)
+  })
+
+  const prevPageClick = () => {
+    if (currentPage.value > 1) {
+      currentPage.value--
+    }
+  }
+
+  const nextPageClick = () => {
+    if (currentPage.value - 1 < computedTotalPages.value) {
+      currentPage.value++
+    }
+  }
+
+  const onInputChange = (input: iInput) => {
+    const inputPage = input.value
+
+    if (Number(inputPage) > computedTotalPages.value) {
+      searchValue.value = computedTotalPages.value.toString()
+    }
+  }
+
+  const onInputBlur = (input: iInput) => {
+    currentPage.value = Number(input.value)
+  }
+
+  const onChangeCount = (val: string) => {
+    itemsCount.value = parseInt(val)
+
+    if (currentPage.value > computedTotalPages.value) {
+      currentPage.value = computedTotalPages.value
+    }
+  }
+
+  return {
+    currentPage,
+    totalCountPages,
+    itemsCount,
+    computedTotalPages,
+    searchValue,
+    prevPageClick,
+    nextPageClick,
+    onInputChange,
+    onInputBlur,
+    onChangeCount,
+  }
 }
