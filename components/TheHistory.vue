5cdf15c14bff066b5b47203786301b5c7ace3f98
letmewin22 <lkosteckiy5@gmail.com>
2024-06-13 09:58:23 +0300

    feat: add pagination and sort to history table

--- components/TheHistory.vue
+++ components/TheHistory.vue
@@ -1,78 +1,48 @@
 <script setup lang="ts">
 import { getLogs } from '~/api/logs/getLogs'
-import type { iInput } from '~/types'
 import type { iLogItem } from '~/types/logs'
 
 const logsList = ref<iLogItem[]>([])
-const currentPage = ref(1)
-const totalCountPages = ref(0)
-const itemsCount = ref(25)
-const searchValue = ref<string>('1')
+const sortedBy = ref('id')
+const sortedOrder = ref<1 | 2>(1)
 
-const computedTotalPages = computed(() => {
-  return Math.floor(totalCountPages.value / itemsCount.value)
-})
+const {
+  currentPage,
+  itemsCount,
+  searchValue,
+  totalCountPages,
+  nextPageClick,
+  prevPageClick,
+  onInputBlur,
+  onInputChange,
+  onChangeCount,
+} = usePagination()
 
-const prevPageClick = () => {
-  if (currentPage.value > 1) {
-    currentPage.value--
-  }
-}
-
-const nextPageClick = () => {
-  if (currentPage.value < computedTotalPages.value) {
-    currentPage.value++
-  }
-}
-
-const selectItem = (val: string) => {
-  itemsCount.value = Number(val)
-}
-
-const onChange = (input: iInput) => {
-  const inputPage = input.value
-
-  if (Number(inputPage) > computedTotalPages.value) {
-    searchValue.value = computedTotalPages.value.toString()
-  }
-}
-
-const onBlur = (input: iInput) => {
-  currentPage.value = Number(input.value)
-}
-
-watch([currentPage, itemsCount], async () => {
-  let offset = 1
-
-  if (currentPage.value > 1) {
-    offset = currentPage.value * itemsCount.value
-  }
-
-  if (offset > totalCountPages.value) {
-    offset = totalCountPages.value - 1
-  }
-
-  console.log(offset, totalCountPages.value)
-
-  const { logs } = await getLogs(offset, itemsCount.value, 'message', 1)
-
-  logsList.value = logs
-})
-
-onMounted(async () => {
+const fetchLogs = async () => {
   const { logs, totalCount } = await getLogs(
-    currentPage.value,
+    currentPage.value - 1,
     itemsCount.value,
-    'message',
-    1
+    sortedBy.value,
+    sortedOrder.value
   )
 
   logsList.value = logs
   totalCountPages.value = totalCount
+}
+
+onMounted(async () => {
+  await fetchLogs()
 })
 
-const onSort = data => {
-  console.log(data)
+watch([currentPage, itemsCount], async () => {
+  await fetchLogs()
+})
+
+const onSort = async (sortState: ISortState) => {
+  sortedBy.value = removeSpaces(formatToSnakeCase(sortState.sortBy))
+  sortedOrder.value = sortState.sortOrder
+
+  await fetchLogs()
 }
 </script>
 
@@ -95,9 +65,9 @@ const onSort = data => {
       input-name="History table navigation"
       @next-click="nextPageClick"
       @prev-click="prevPageClick"
-      @selected-item="selectItem"
-      @on-blur-value="onBlur"
-      @on-change-value="onChange"
+      @selected-item="onChangeCount"
+      @on-blur-value="onInputBlur"
+      @on-change-value="onInputChange"
     />
   </div>
 </template>
