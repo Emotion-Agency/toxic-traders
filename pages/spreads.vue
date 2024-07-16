3e236f4601195a71dae5db97f0b238bcc5024a33
Valeriy-Gnybidenko <valeriy.gnybidenko@gmail.com>
2024-07-16 16:12:40 +0300

    feat: add symbols names to spreads

--- pages/spreads.vue
+++ pages/spreads.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+import type { iSpreadsInput } from '~/types/spreads/spreads'
+
+const inputsList = ref<iSpreadsInput[]>([])
+const symbolsNames = ref([])
+const filteredSymbolsNames = ref([])
+const spreadsList = ref([])
+const selectedSymbol = ref('')
+
 const {
   getServerAccountSymbolsSpreads,
   getServerAccountSymbolsNames,
@@ -22,37 +30,61 @@ const spreadsParams = ref({
   sortOrder: 1,
 })
 
-const selectData = ref([
-  {
-    options: ['Option 1', 'Option 2', 'Option 3'],
-    placeholder: 'Select symbol',
-    title: 'Sumbol',
-    searchInput: {
-      id: 'spreads-search-symbol',
-      required: false,
-      name: 'Spreads search symbol',
-      type: 'text',
-      placeholder: 'Search symbol',
-      isRightButton: true,
-    },
-  },
-  {
-    options: ['Option 1', 'Option 2', 'Option 3'],
-    placeholder: 'Select platform',
-    title: 'Platforms',
-    searchInput: {
-      id: 'spreads-search-platform',
-      required: false,
-      name: 'Spreads search platform',
-      type: 'text',
-      placeholder: 'Search platform',
-      isRightButton: true,
-    },
-  },
-])
+watch(
+  () => [symbolsNames.value, filteredSymbolsNames.value],
+  () => {
+    inputsList.value = [
+      {
+        options: filteredSymbolsNames.value,
+        placeholder: 'Select symbol',
+        title: 'Sumbol',
+        searchInput: {
+          id: 'spreads-search-symbol',
+          required: false,
+          name: 'Spreads search symbol',
+          type: 'text',
+          placeholder: 'Search symbol',
+          isRightButton: true,
+        },
+      },
+      {
+        title: 'Description',
+        required: false,
+        id: 'spreads-description',
+        name: 'Spreads description',
+        type: 'text',
+        value: '',
+        placeholder: 'Write your desription',
+        isRightButton: true,
+      },
+      {
+        options: ['Option 1', 'Option 2', 'Option 3'],
+        placeholder: 'Select platform',
+        title: 'Platforms',
+        searchInput: {
+          id: 'spreads-search-platform',
+          required: false,
+          name: 'Spreads search platform',
+          type: 'text',
+          placeholder: 'Search platform',
+          isRightButton: true,
+        },
+      },
+    ]
+  }
+)
 
-const searchItems = (val: string) => {
-  console.log(val)
+const searchSymbolsName = (searchValue: string) => {
+  filteredSymbolsNames.value = symbolsNames.value?.filter(
+    item =>
+      item.toLowerCase().trim().includes(searchValue.toLowerCase().trim()) &&
+      item !== selectedSymbol.value
+  )
+}
+
+const selectSymbolsName = (item: string) => {
+  selectedSymbol.value = item
+  filteredSymbolsNames.value = symbolsNames.value?.filter(name => name !== item)
 }
 
 // const onSorted = (sortState: ISortState) => {
@@ -71,10 +103,13 @@ const searchItems = (val: string) => {
 
 onMounted(async () => {
   const data = await getServerAccountSymbolsSpreads(params.value)
-  const names = await getServerAccountSymbolsNames()
-  const data2 = await getServerAccountSymbolsSpreadsAll(spreadsParams.value)
+  symbolsNames.value = await getServerAccountSymbolsNames()
+  filteredSymbolsNames.value = symbolsNames.value
+  spreadsList.value = await getServerAccountSymbolsSpreadsAll(
+    spreadsParams.value
+  )
 
-  console.log(data, names, data2)
+  console.log(data, symbolsNames.value, spreadsList.value)
 })
 </script>
 
@@ -86,22 +121,40 @@ onMounted(async () => {
         <div class="spreads__content">
           <ul class="spreads__select-list">
             <li
-              v-for="(item, idx) in selectData"
+              v-for="(item, idx) in inputsList"
               :key="idx"
               class="spreads__select-item"
             >
               <CustomSelect
+                v-if="item.options"
                 :options="item.options"
                 :search-input="item.searchInput"
                 :placeholder="item.placeholder"
                 :title="item.title"
                 class="main-broker__settings-select"
-                @search="searchItems"
+                @search="searchSymbolsName"
+                @select="selectSymbolsName"
               >
                 <template #right-icon>
                   <IconsSearch />
                 </template>
               </CustomSelect>
+              <TheInput
+                v-else
+                :id="item.id"
+                :required="item.required"
+                :title="item.title"
+                :name="item.name"
+                :type="item.type"
+                :placeholder="item.placeholder"
+                :value="item.value"
+                :is-right-button="item.isRightButton"
+                class="main-broker__settings-input"
+              >
+                <template #right-icon>
+                  <IconsSearch />
+                </template>
+              </TheInput>
             </li>
           </ul>
         </div>
