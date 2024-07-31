2bfb5214b4d5cdcfbda55a37a3b4e851daddc0ed
letmewin22 <lkosteckiy5@gmail.com>
2024-07-31 16:28:09 +0300

    fix: modal height detection

--- components/TheModal.vue
+++ components/TheModal.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
-import { raf } from '~/utils/ea'
-
 interface IProps {
   modalOpened: boolean
   title?: string
   className?: string
 }
 
-const props = defineProps<IProps>()
+defineProps<IProps>()
 
 const $modal = ref<HTMLElement | null>(null)
 const $modalContainer = ref<HTMLElement | null>(null)
-const isFullHeight = ref(false)
 
 const emit = defineEmits(['close'])
 
@@ -19,26 +16,6 @@ const onClose = () => {
   emit('close')
   document.body.classList.remove('modal-open')
 }
-
-const detectModalContentHeight = () => {
-  const wh = window.innerHeight
-  const modalContentHeight = $modalContainer.value.scrollHeight
-
-  isFullHeight.value = modalContentHeight >= wh
-}
-
-watch(
-  () => props.modalOpened,
-  () => {
-    if (props.modalOpened) {
-      raf.on(detectModalContentHeight)
-    }
-  }
-)
-
-onBeforeUnmount(() => {
-  raf.off(detectModalContentHeight)
-})
 </script>
 
 <template>
@@ -49,10 +26,7 @@ onBeforeUnmount(() => {
       :class="[modalOpened && 'modal--opened', className]"
     >
       <div class="modal__backdrop" @click="onClose" />
-      <div
-        class="modal__content"
-        :class="isFullHeight && 'modal__content--full-height'"
-      >
+      <div class="modal__content">
         <div class="modal__header">
           <p v-if="title" class="modal__title">
             {{ title }}
