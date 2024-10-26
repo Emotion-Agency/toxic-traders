bd0db296574408da738bba102222fe3e255a24d7
valeriy-developer <valeriy.gnybidenko@gmail.com>
2024-10-26 23:43:18 +0300

    feat: updated settings layout

--- layouts/settings.vue
+++ layouts/settings.vue
@@ -1,9 +1,51 @@
+<script setup lang="ts">
+const navigationList = [
+  {
+    text: 'Account',
+    link: '/settings/account',
+  },
+  {
+    text: 'Calendar',
+    link: '/settings/calendar',
+  },
+  {
+    text: 'Schedule news',
+    link: '/settings/schedule-news',
+  },
+  {
+    text: 'Users',
+    link: '/settings/users',
+  },
+  {
+    text: 'Servers',
+    link: '/settings/servers',
+  },
+]
+</script>
+
 <template>
   <NuxtLayout name="default">
-    <div class="settings" style="padding-top: 120px">
-      <h1>Settings</h1>
-      <hr />
-      <slot />
+    <div class="container settings" style="padding-top: 120px">
+      <h1 class="settings__title">Settings</h1>
+      <div class="settings__divider" />
+      <div class="settings__wrapper">
+        <nav class="settings__nav">
+          <ul class="settings__nav-list">
+            <li
+              v-for="(item, idx) in navigationList"
+              :key="idx"
+              class="settings__nav-item"
+            >
+              <NuxtLink :to="item.link" class="settings__nav-link">
+                {{ item.text }}
+              </NuxtLink>
+            </li>
+          </ul>
+        </nav>
+        <div class="settings__content-wrapper">
+          <slot />
+        </div>
+      </div>
     </div>
   </NuxtLayout>
 </template>
---
+++ pages/settings/schedule-news.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>Schedule News</div>
+</template>
