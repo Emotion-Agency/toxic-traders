579dc44b1e53f3d14b3763f3b8008fa5d353c14f
letmewin22 <lkosteckiy5@gmail.com>
2024-11-26 10:02:41 +0200

    feat: improve pagination

--- pages/settings/calendar.vue
+++ pages/settings/calendar.vue
@@ -17,6 +17,24 @@ const symbols = ref([])
 const filteredSymbols = ref([])
 const selectedSymbol = ref<string>(null)
 
+const route = useRoute()
+const router = useRouter()
+
+const {
+  currentPage,
+  itemsCount,
+  searchValue,
+  totalCountPages,
+  nextPageClick,
+  prevPageClick,
+  onInputBlur,
+  onInputChange,
+  onChangeCount,
+} = usePagination(
+  route.query.page ? Number(route.query.page) : 1,
+  route.query.count ? Number(route.query.count) : 100
+)
+
 const symbolsSelect = computed(() => {
   return {
     options: filteredSymbols.value,
@@ -69,10 +87,31 @@ const { getSymbols } = useSymbols()
 onMounted(async () => {
   try {
     isLoading.value = true
-    await getAllEventsByPage(1, 25)
+    await getAllEventsByPage(currentPage.value, itemsCount.value)
+    totalCountPages.value = 1000
     countries.value = await getFlags()
     symbols.value = await getSymbols()
     filteredSymbols.value = symbols.value
+  } catch (error) {
+    console.error(error)
+  } finally {
+    isLoading.value = false
+  }
+})
+
+watch([currentPage, itemsCount], async () => {
+  router.push({
+    query: {
+      ...route.query,
+      page: currentPage.value,
+      count: itemsCount.value,
+    },
+  })
+
+  document.documentElement.scrollTop = 0
+  try {
+    isLoading.value = true
+    await getAllEventsByPage(currentPage.value, itemsCount.value)
   } catch (error) {
     console.error(error)
   } finally {
@@ -103,79 +142,92 @@ onMounted(async () => {
       </InputField>
     </div>
     <UiLoader v-if="isLoading" />
-    <ul
-      v-else-if="!isLoading && events?.length"
-      class="settings-calendar__items"
-    >
-      <li
-        v-for="event of events"
-        :key="event.id"
-        class="settings-calendar__item sci"
-      >
-        <h3 class="sci__title">
-          <span>
-            <img
-              v-if="getCountryFlag(event.country)?.url"
-              class="sci__flag"
-              :src="getCountryFlag(event.country)?.url"
-              :alt="getCountryFlag(event.country).alt"
-            />
-          </span>
-          <b> {{ event.country }}</b> <span>{{ event.event }}</span>
-        </h3>
-        <InputCheckbox
-          :id="`sci--${event.id}`"
-          name="sci"
-          title="sci"
-          class="sci__checkbox"
-          value="Disable"
-          @input-value="onDisable($event, event.id)"
-        />
-        <InputSelect
-          :id="`sci-imp--${event.id}`"
-          placeholder="Choose importance"
-          :options="['Low', 'Medium', 'High']"
-          title="Importance"
-          class="sci__select sci__importance"
-          @select="onImportanceChange($event, event.id)"
+    <div v-if="events?.length">
+      <ul class="settings-calendar__items">
+        <li
+          v-for="event of events"
+          :key="event.id"
+          class="settings-calendar__item sci"
         >
-          <InputSelectOption
-            v-for="(option, idx) in ['Low', 'Medium', 'High']"
-            :key="option"
-            :option="option"
-            :index="idx"
+          <h3 class="sci__title">
+            <span>
+              <img
+                v-if="getCountryFlag(event.country)?.url"
+                class="sci__flag"
+                :src="getCountryFlag(event.country)?.url"
+                :alt="getCountryFlag(event.country).alt"
+              />
+            </span>
+            <b> {{ event.country }}</b> <span>{{ event.event }}</span>
+          </h3>
+          <InputCheckbox
+            :id="`sci--${event.id}`"
+            name="sci"
+            title="sci"
+            class="sci__checkbox"
+            value="Disable"
+            @input-value="onDisable($event, event.id)"
+          />
+          <InputSelect
+            :id="`sci-imp--${event.id}`"
+            placeholder="Choose importance"
+            :options="['Low', 'Medium', 'High']"
+            title="Importance"
+            class="sci__select sci__importance"
+            @select="onImportanceChange($event, event.id)"
           >
-            {{ option }}
-          </InputSelectOption>
-        </InputSelect>
-        <InputSelect
-          :search-input="{
-            ...symbolsSelect.searchInput,
-            id: `sci-symbol-search--${event.id}`,
-          }"
-          :id="`sci-symbol--${event.id}`"
-          :placeholder="symbolsSelect.placeholder"
-          :options="symbolsSelect.options"
-          :title="symbolsSelect.title"
-          class="sci__select sci__symbol"
-          v-slot="{ renderedItems }"
-          @select="onSymbolChange($event, event.id)"
-          @search="onSymbolSearch"
-        >
-          <InputSelectOption
-            v-for="(option, idx) in renderedItems"
-            :key="option"
-            :option="option"
-            :index="idx"
+            <InputSelectOption
+              v-for="(option, idx) in ['Low', 'Medium', 'High']"
+              :key="option"
+              :option="option"
+              :index="idx"
+            >
+              {{ option }}
+            </InputSelectOption>
+          </InputSelect>
+          <InputSelect
+            :search-input="{
+              ...symbolsSelect.searchInput,
+              id: `sci-symbol-search--${event.id}`,
+            }"
+            :id="`sci-symbol--${event.id}`"
+            :placeholder="symbolsSelect.placeholder"
+            :options="symbolsSelect.options"
+            :title="symbolsSelect.title"
+            class="sci__select sci__symbol"
+            v-slot="{ renderedItems }"
+            @select="onSymbolChange($event, event.id)"
+            @search="onSymbolSearch"
           >
-            {{ option }}
-          </InputSelectOption>
-        </InputSelect>
-        <TheButton class="sci__button" button-size="small">
-          Reactions Settings
-        </TheButton>
-      </li>
-    </ul>
-    <NotFound v-else />
+            <InputSelectOption
+              v-for="(option, idx) in renderedItems"
+              :key="option"
+              :option="option"
+              :index="idx"
+            >
+              {{ option }}
+            </InputSelectOption>
+          </InputSelect>
+          <TheButton class="sci__button" button-size="small">
+            Reactions Settings
+          </TheButton>
+        </li>
+      </ul>
+      <ThePagination
+        :currentPage="currentPage"
+        :itemsCount="itemsCount"
+        :total-pages="totalCountPages"
+        input-id="settings-calendar-pagination"
+        input-name="Settings calendar pagination"
+        :options="['25', '50', '100']"
+        :input-value="searchValue"
+        @next-click="nextPageClick"
+        @prev-click="prevPageClick"
+        @selected-item="onChangeCount"
+        @on-blur-value="onInputBlur"
+        @on-change-value="onInputChange"
+      />
+    </div>
+    <NotFound v-if="!events?.length && !isLoading" />
   </section>
 </template>
