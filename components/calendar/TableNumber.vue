0d3ba823a2780b50cd37f7d0e1ca13d7a4b37128
letmewin22 <lkosteckiy5@gmail.com>
2024-12-16 10:56:30 +0200

    fix: table number

--- components/calendar/TableNumber.vue
+++ components/calendar/TableNumber.vue
@@ -13,7 +13,7 @@ const props = withDefaults(defineProps<IProps>(), {
 })
 
 const styledNumber = computed(() => {
-  if (props.number || props.number === 0) {
+  if (props.number || props.number === '0' || props.number === 0) {
     return Number(props.number).toFixed(1) + props.scale + ' ' + props.unit
   }
   return '-'
