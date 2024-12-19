6cf0a9dcd7218efca651889499000c9f53d7ec22
letmewin22 <lkosteckiy5@gmail.com>
2024-12-19 08:45:43 +0200

    feat: update reactions

--- components/calendar/TableReaction.vue
+++ components/calendar/TableReaction.vue
@@ -5,24 +5,30 @@ interface IProps {
   event: ITableCalendarEvent
 }
 
-defineProps<IProps>()
+const props = defineProps<IProps>()
 
 const onClick = () => {}
+
+const hasReactions = computed(() => {
+  return props.event?.symbols?.some(symbol => symbol.reaction)
+})
 </script>
 
 <template>
-  <HeadlessDropdownMenu
-    class="dropdown"
-    trigger="hover"
-    v-if="event?.firstSymbol?.reaction"
-  >
+  <HeadlessDropdownMenu class="dropdown" trigger="hover" v-if="hasReactions">
     <HeadlessDropdownTrigger
       class="calendar-table__reaction"
+      :class="{
+        'calendar-table__reaction--active': event?.firstSymbol?.reaction,
+      }"
       @click.stop.prevent="onClick"
     >
-      <span class="header__account-name">{{
+      <span v-if="event?.firstSymbol?.reaction" class="header__account-name">{{
         event?.firstSymbol?.reaction
       }}</span>
+      <span v-else>
+        <IconsInfoCircle />
+      </span>
     </HeadlessDropdownTrigger>
     <Teleport to="#teleports">
       <Transition name="dropdown">
