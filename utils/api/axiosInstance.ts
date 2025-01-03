2c4568afc8c0a9881f3947969f11e73564e24e1d
letmewin22 <lkosteckiy5@gmail.com>
2025-01-03 08:54:05 +0200

    fix: increase axios timeout

--- utils/api/axiosInstance.ts
+++ utils/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   baseURL: 'https://toxictraders.com:5001/api',
-  timeout: 20000,
+  timeout: 60000,
   headers: {
     'Content-Type': 'application/json',
     accept: '*/*',
