61ea4fcd301d040b957d75f27242e86133aafb38
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-04-30 14:59:13 +0300

    feat: added accounts hero sections

--- pages/trading/accounts.vue
+++ pages/trading/accounts.vue
@@ -2,8 +2,35 @@
 
 <template>
   <main>
-    <section class="accounts">
-      <div class="accounts-wrapper container">Accounts page</div>
+    <section class="hero-accounts">
+      <div class="container hero-accounts__wrapper">
+        <h1 class="hero-accounts__title">Accounts</h1>
+        <div class="hero-accounts__btns">
+          <TheButton tag="button" class="hero-accounts__btn">
+            <template #start-icon>
+              <IconsPlus />
+            </template>
+            Add account
+          </TheButton>
+          <TheButton tag="button" variant="outlined" class="hero-accounts__btn">
+            <template #start-icon>
+              <IconsSuccess />
+            </template>
+            Check accounts
+          </TheButton>
+          <TheButton
+            tag="button"
+            variant="outlined"
+            class="hero-accounts__btn"
+            disabled
+          >
+            <template #start-icon>
+              <IconsPlus />
+            </template>
+            Add investor account
+          </TheButton>
+        </div>
+      </div>
     </section>
   </main>
 </template>
