c3f11a1dd224ec6db13d8d4eefb6bc9eaac09c8b
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-05-30 00:08:18 +0300

    feat: added events panel menu

---
+++ components/trading/trader/events-menu/EventsCircles.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="events-circles">
+    <button
+      type="button"
+      class="events-circles__circle events-circles__circle--warning"
+    >
+      <p class="events-circles__num">10</p>
+    </button>
+    <button
+      type="button"
+      class="events-circles__circle events-circles__circle--success"
+    >
+      <p class="events-circles__num">15</p>
+    </button>
+  </div>
+</template>
---
+++ components/trading/trader/events-menu/EventsMenu.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="events-menu">
+    <div class="events-menu__wrapper">
+      <TradingTraderEventsMenuEventsCircles />
+      <div class="events-menu__btns">
+        <TheButton
+          tag="button"
+          button-size="small"
+          variant="fill"
+          class="events-menu__btn events-menu__btn--event"
+        >
+          <template #start-icon>
+            <IconsPlus />
+          </template>
+          New event
+        </TheButton>
+        <TheButton
+          tag="button"
+          button-size="small"
+          variant="outlined"
+          class="events-menu__btn events-menu__btn--percent"
+        >
+          100%
+        </TheButton>
+        <TheButton
+          tag="button"
+          button-size="small"
+          variant="outlined"
+          class="events-menu__btn events-menu__btn--terminal"
+        >
+          <template #start-icon>
+            <IconsTerminal />
+          </template>
+          Add terminals
+        </TheButton>
+        <TheButton
+          tag="button"
+          button-size="small"
+          variant="soft"
+          class="events-menu__btn events-menu__btn--settings"
+        >
+          Load settings
+        </TheButton>
+        <TheButton
+          tag="button"
+          button-size="small"
+          variant="fill"
+          class="events-menu__btn events-menu__btn--chart"
+        >
+          <template #start-icon>
+            <IconsGraph />
+          </template>
+          Hide Chart
+        </TheButton>
+      </div>
+    </div>
+  </div>
+</template>
