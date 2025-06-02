f6a2c5f8efcd876bf0231e294814e229e6d0a8c5
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-06-02 18:45:47 +0300

    feat: added loader to events

--- components/trading/trader/events-menu/EventsCircles.vue
+++ components/trading/trader/events-menu/EventsCircles.vue
@@ -3,14 +3,29 @@ import type { IEvent } from '~/types/events/events'
 
 const { getAllEvents } = useEvents()
 const events = ref<IEvent[]>([])
+const isLoading = ref(false)
 
 onMounted(async () => {
-  events.value = await getAllEvents()
+  try {
+    isLoading.value = true
+    events.value = await getAllEvents()
+  } catch (error) {
+    console.error('Failed to fetch events:', error)
+  } finally {
+    isLoading.value = false
+  }
 })
 </script>
 
 <template>
   <div class="events-circles">
-    <TradingTraderEventsMenuEventsDropdown :events="events" />
+    <UiLoader v-if="isLoading" class="events-circles__loader" />
+    <TradingTraderEventsMenuEventsDropdown
+      v-else-if="events?.length"
+      :events="events"
+    />
+    <div v-else class="events-circles__empty">
+      <p class="events-circles__empty-text">No events found</p>
+    </div>
   </div>
 </template>
